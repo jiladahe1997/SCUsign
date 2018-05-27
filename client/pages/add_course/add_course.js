6d34cf85d4779696bede6104602edbf63fe815e1
mac <mac@macdeAir.lan>
2018-05-27 22:40:44 +0800

    增加了添加课程界面，新建开发分支
    增加来添加课程界面

--- client/pages/add_course/add_course.js
+++ client/pages/add_course/add_course.js
@@ -9,6 +9,7 @@ Page({
   data: {
   
   },
+  
 
   /**
    * 生命周期函数--监听页面加载
@@ -107,5 +108,11 @@ Page({
         })
       }
     })
-  }
+  },
+
+   backToIndex() {
+    wx.redirectTo({
+      url: `../index/index?identity=${this.data.identity}`
+    })
+  },
 })
